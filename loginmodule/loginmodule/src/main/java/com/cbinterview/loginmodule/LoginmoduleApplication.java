b53df6cc6b4676408f16b74899ff97e122572233
sundar93 <sundar9393@gmail.com>
2020-10-19 22:30:27 +0530

    first commit

---
+++ loginmodule/loginmodule/src/main/java/com/cbinterview/loginmodule/LoginmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.cbinterview.loginmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginmoduleApplication.class, args);
+	}
+
+}
---
+++ loginmodule/loginmodule/src/test/java/com/cbinterview/loginmodule/LoginmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbinterview.loginmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
