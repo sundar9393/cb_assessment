2950c442cb7f7891ac01e0a5fc5ba6a252ee9750
sundar93 <sundar9393@gmail.com>
2020-10-22 19:05:32 +0530

    ui logic

--- loginmodule/loginmodule/login-api/src/main/java/app/login/api/config/WebConfig.java
+++ loginmodule/loginmodule/login-api/src/main/java/app/login/api/config/WebConfig.java
@@ -1,9 +1,11 @@
 package app.login.api.config;
 
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("app.login.api.controller")
+@ServletComponentScan("app.login.api.filters")
 public class WebConfig {
 }
